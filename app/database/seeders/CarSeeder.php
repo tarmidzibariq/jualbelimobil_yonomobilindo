7cebc3f573fceca82f81d01bb76706f396399e51
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-06-16 22:27:21 +0700

    Add Dhaihatsu Ayla entry to CarSeeder with updated description and photo association

--- app/database/seeders/CarSeeder.php
+++ app/database/seeders/CarSeeder.php
@@ -46,6 +46,39 @@ class CarSeeder extends Seeder
                 'number' => $i,
             ]);
         }
+
+        $car = Car::create([
+            'user_id' => 1,
+            'brand' => 'Dhaihatsu',
+            'model' => 'Ayla',
+            'year' => '2021',
+            'price' => '90000000',
+            'transmission' => 'automatic',
+            'description' => 'Toyota Alphard menjadi salah satu mobil MPV mewah dengan angka penjualan yang cukup tinggi.
+                Ukuran yang besar dengan interior yang mewah menjadikan mobil ini sebagai mobil standar bagi
+                kalangan menengah atas di Indonesia. Segera jadwalkan untuk informasi lebih lanjut.',
+            'service_history' => '2024-05-20',
+            'fuel_type' => 'bensin',
+            'mileage' => '100000',
+            'color' => 'silver',
+            'tax' => '2025-05-20',
+            'engine' => '2500',
+            'bpkb'=> true,
+            'spare_key'=> true,
+            'manual_book'=> true,
+            'service_book'=> true,
+            'sale_type' => 'showroom',
+            'seat' => '4',
+            'status' => 'available',
+        ]);
+
+        foreach (range(1, 5) as $i) {
+            CarPhoto::create([
+                'car_id' => $car->id,
+                'photo_url' => "$i.jpg",
+                'number' => $i,
+            ]);
+        }
     }
 
 }
