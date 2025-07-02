81852d16c5957c556565807dca6c06349345a944
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-07-02 22:09:26 +0700

    Add favicon link to registration page for improved branding

--- app/resources/views/web/auth/register.blade.php
+++ app/resources/views/web/auth/register.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Form Daftar YONOMOBILINDO</title>
 
+    {{-- icon --}}
+    <link rel="icon" type="image/png" href="{{ asset('image/YONO-MOBILINDO-LOGO.svg') }}">
+
     <!-- font montserat -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
