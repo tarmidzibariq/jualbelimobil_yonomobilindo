a1a52db99c2fc71da23dcf1e3f9c504d22077913
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-07-18 23:00:35 +0700

    Comment out Vite asset loading and add Bootstrap CDN link in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,8 @@
     <link rel="icon" type="image/png" href="{{ asset('image/YONO-MOBILINDO-LOGO.svg') }}">
 
     <!-- Bootstrap & Vite -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- font montserat -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
